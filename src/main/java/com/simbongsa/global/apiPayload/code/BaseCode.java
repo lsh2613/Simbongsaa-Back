618cb5a4550ce5b914ba2e36b32722b3fa8ee0b6
LSH <104637774+lsh2613@users.noreply.github.com>
2024-05-24 21:38:55 +0900

    first commit

---
+++ src/main/java/com/simbongsa/global/apiPayload/code/BaseCode.java
@@ -0,0 +1,9 @@
+package com.simbongsa.global.apiPayload.code;
+
+public interface BaseCode {
+
+    public ResponseDTO getDto();
+
+    public ResponseDTO getHttpStatusDto();
+
+}
---
+++ src/test/java/com/simbongsa/SimbongsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simbongsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbongsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
