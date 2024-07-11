ca661bc3ee0dc24a7c0644efbe76161610da8b77
LSH <104637774+lsh2613@users.noreply.github.com>
2024-07-12 01:50:46 +0900

    [feat] 유저 CRUD 구현

---
+++ src/main/java/com/simbongsa/member/service/MemberServiceImpl.java
@@ -0,0 +1,61 @@
+package com.simbongsa.member.service;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.simbongsa.global.EntityFacade;
+import com.simbongsa.global.common.apiPayload.code.statusEnums.ErrorStatus;
+import com.simbongsa.global.common.exception.GeneralHandler;
+import com.simbongsa.global.jwt.service.TokenProvider;
+import com.simbongsa.global.redis.util.RedisUtil;
+import com.simbongsa.member.entity.Member;
+import com.simbongsa.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Date;
+
+import static com.simbongsa.global.common.apiPayload.code.statusEnums.ErrorStatus.TOKEN_ID_MISMATCH;
+
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class MemberServiceImpl implements MemberService {
+    private final MemberRepository memberRepository;
+    private final EntityFacade entityFacade;
+    private final TokenProvider tokenProvider;
+    private final RedisUtil redisUtil;
+
+    @Override
+    public void deleteMember(Long loginMemberId, Long memberId) {
+        Member member = entityFacade.getMember(memberId);
+        isAuthorizedMember(loginMemberId, memberId);
+        memberRepository.delete(member);
+    }
+
+    private void isAuthorizedMember(Long loginMemberId, Long memberId) {
+        if (!loginMemberId.equals(memberId)) {
+            throw new GeneralHandler(ErrorStatus._UNAUTHORIZED);
+        }
+
+    }
+
+    @Override
+    public void logout(String accessToken, String refreshToken, Long memberId) {
+        DecodedJWT decodedAccessToken = tokenProvider.decodedJWT(accessToken);
+        Long accessTokenId = decodedAccessToken.getClaim("id").asLong();
+
+        DecodedJWT decodedRefreshToken = tokenProvider.decodedJWT(refreshToken);
+        Long refreshTokenId = decodedRefreshToken.getClaim("id").asLong();
+
+        if (accessTokenId != memberId || refreshTokenId != memberId) {
+            throw new GeneralHandler(TOKEN_ID_MISMATCH);
+        }
+
+        Date expiresAt = decodedAccessToken.getExpiresAt();
+        long diff = expiresAt.getTime() - System.currentTimeMillis();
+        redisUtil.setBlackList(accessToken, accessTokenId, diff);
+
+        redisUtil.deleteData(refreshToken);
+    }
+}
