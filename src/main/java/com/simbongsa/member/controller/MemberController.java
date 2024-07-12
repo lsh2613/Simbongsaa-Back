8e79ee34926976a22ef2b06ba454028284d874b4
LSH <104637774+lsh2613@users.noreply.github.com>
2024-07-12 11:42:39 +0900

    [refactor] 유저 requestMapping URL 수정

--- src/main/java/com/simbongsa/member/controller/MemberController.java
+++ src/main/java/com/simbongsa/member/controller/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
